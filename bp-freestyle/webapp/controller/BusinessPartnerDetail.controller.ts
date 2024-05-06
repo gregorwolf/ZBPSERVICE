c7328282ce856f0ee458cfbb71c308eec4737eaf
Gregor Wolf <gregor.wolf@gmail.com>
2024-05-06 18:15:59 +0200

    fix TypeScript issues

--- bp-freestyle/webapp/controller/BusinessPartnerDetail.controller.ts
+++ bp-freestyle/webapp/controller/BusinessPartnerDetail.controller.ts
@@ -1,6 +1,6 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import UIComponent from "sap/ui/core/UIComponent";
-import { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
+import Route$PatternMatchedEvent from "sap/ui/core/routing/Route";
 /**
  * @namespace csw.bpfreestyle.controller
  */
@@ -9,31 +9,44 @@ export default class BusinessPartnerDetail extends Controller {
   public onInit(): void {
     const oOC = this.getOwnerComponent() as UIComponent;
     const oRouter = oOC.getRouter();
-    oRouter
-      .getRoute("RouteBusinessPartnerDetail")
-      .attachPatternMatched(this._onRouteMatched, this);
+    const route = oRouter.getRoute("RouteBusinessPartnerDetail");
+    if (route) {
+      route.attachPatternMatched(this._onRouteMatched, this);
+    } else {
+      console.error('Route "RouteBusinessPartnerDetail" is not defined');
+    }
   }
 
   public onTakePicture(): void {
     // eslint-disable-next-line no-console
     console.log("Take Picture");
     var oPicPreviewID = this.createId("picPreview");
-    //trigger click event for the input field to open camera
-    var image = document.getElementById(this.createId("file"));
-    image.addEventListener("change", function () {
-      //check if there is an image
-      if (this.files && this.files[0]) {
-        var previewPic = document.getElementById(oPicPreviewID);
-        var reader = new FileReader();
-        reader.onload = function (e) {
-          previewPic.src = e.target.result;
-          pictureObj = e.target.result;
-        };
+    var fileId = this.createId("file");
 
-        reader.readAsDataURL(this.files[0]);
+    if (oPicPreviewID && fileId) {
+      //trigger click event for the input field to open camera
+      var image = document.getElementById(fileId);
+      if (image !== null) {
+        image.addEventListener("change", function () {
+          //check if there is an image
+          if (this.files && this.files[0]) {
+            var previewPic = document.getElementById(oPicPreviewID);
+            var reader = new FileReader();
+            reader.onload = function (e) {
+              if (previewPic) {
+                previewPic.src = e.target.result;
+                pictureObj = e.target.result;
+              }
+            };
+
+            reader.readAsDataURL(this.files[0]);
+          }
+        });
+        image.click();
       }
-    });
-    image.click();
+    } else {
+      console.error("IDs are not defined");
+    }
   }
 
   private _onRouteMatched(event: Route$PatternMatchedEvent): void {
