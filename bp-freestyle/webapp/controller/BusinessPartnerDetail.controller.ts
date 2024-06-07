d7b88cabc763f2c4f0640766c3ac1bc48aae9c7f
lemaiwo <wouterlemaire120@hotmail.com>
2024-06-07 20:03:47 +0200

    add file upload

--- bp-freestyle/webapp/controller/BusinessPartnerDetail.controller.ts
+++ bp-freestyle/webapp/controller/BusinessPartnerDetail.controller.ts
@@ -1,8 +1,12 @@
+/* eslint-disable linebreak-style */
 import Controller from "sap/ui/core/mvc/Controller";
 import UIComponent from "sap/ui/core/UIComponent";
 import { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
 import { BPAddress } from "../../gen/ZbpSrvModel";
 import JSONModel from "sap/ui/model/json/JSONModel";
+import ODataModel from "sap/ui/model/odata/v2/ODataModel";
+import FileUploader, { FileUploader$ChangeEvent, FileUploader$UploadStartEvent } from "sap/ui/unified/FileUploader";
+import FileUploaderParameter from "sap/ui/unified/FileUploaderParameter";
 /**
  * @namespace csw.bpfreestyle.controller
  */
@@ -26,6 +30,23 @@ export default class BusinessPartnerDetail extends Controller {
   }
 
   public onUploadPicture(): void {
+    const fileuploader = (this.byId("fileuploader") as FileUploader);
+    
+    fileuploader.removeAllHeaderParameters();
+    // fileUpload.removeHeaderParameter("x-csrf-token");
+    // Header Token
+    const customerHeaderToken = new FileUploaderParameter({
+      name: "x-csrf-token",
+      value: (this.getView()?.getModel() as ODataModel).getSecurityToken()
+    });
+    fileuploader.addHeaderParameter(customerHeaderToken);
+    var customerHeaderSlug = new FileUploaderParameter({
+      name: "slug",
+      value: fileuploader.getValue()
+    });
+    fileuploader.addHeaderParameter(customerHeaderSlug);
+    
+    fileuploader.upload();
     // eslint-disable-next-line no-console
     console.log("Upload Picture");
   }
@@ -77,13 +98,46 @@ export default class BusinessPartnerDetail extends Controller {
       return;
     }
     const oObject: BPAddress = objectOrUndefined;
-
+    const path = (this.getView()?.getModel() as ODataModel).createKey("/BPAddressSet",{
+      Partner:window.decodeURIComponent(oObject.Partner)
+    });
     this.getView()?.bindElement({
-      path:
-        "/BPAddressSet('" + window.decodeURIComponent(oObject.Partner) + "')",
+      path:path,
       parameters: {
         expand: "to_Attachments",
       },
     });
+  }
+  private onBeforeUploadStarts(event:FileUploader$UploadStartEvent) {
+    const fileUpload = event.getSource();
+    // fileUpload.removeHeaderParameter("slug");
+    // Header Slug
+    // var customerHeaderSlug = new FileUploaderParameter({
+    // 	name: "slug",
+    // 	value: oEvent.getParameter("fileName")
+    // });
+    // fileUpload.addHeaderParameter(customerHeaderSlug);
+  
+    this.getView()?.setBusy(true);
+  }
+  private onUploadComplete(){
+    this.getView()?.setBusy(false);
+  }
+  private onUploadChange(event:FileUploader$ChangeEvent) {
+    const viewModel = this.getView()?.getModel("view") as JSONModel;
+    const files = event?.getParameter("files");
+    if(files){
+      const file = files[0] as Blob;
+      var reader = new FileReader();
+      reader.onload = function (e) {
+          if (e.target?.result !== null) {
+            // pictureObj = e.target.result;
+            viewModel.setProperty("/onUploadPictureVisible", true);
+            viewModel.setProperty("/image", e.target?.result);
+          }
+      };
+
+      reader.readAsDataURL(file);
+    }
   }
 }
