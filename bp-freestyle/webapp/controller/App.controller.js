6bd320cfc956981b7fcdbfe9532a7ade6304b534
Gregor Wolf <gregor.wolf@gmail.com>
2024-12-29 10:26:30 +0100

    generated freestyle JavaScript

---
+++ bp-freestyle/webapp/controller/App.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("csw.bpfreestyle.controller.App", {
+        onInit: function () {
+
+        }
+    });
+});
---
+++ bp-freestyle/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"csw/bp-freestyle/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
